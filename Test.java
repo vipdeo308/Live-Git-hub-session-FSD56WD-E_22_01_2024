702cdb0ae77833c69f1ae0aef550c5bb97b067cc
vipdeo308 <93776366+vipdeo308@users.noreply.github.com>
2024-01-22 11:56:57 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Test
+{
+  public static void main(String[] args)
+  {
+    JOptionPane.showMessageDialog(null,"Welsome to First Java GUI Program!");
+  }
+}
